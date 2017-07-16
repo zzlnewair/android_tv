0590514aca9d1646bbdad920b930d6a8ec2ca624
zzlnewair <zzl2007315@163.com>
2017-07-16 16:06:42 +0800

    TvRecyclerView

---
+++ TvRecyclerView/src/com/zzl/demo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.zzl.demo;
+
+import com.zzl.recyclerview.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    
+}
---
+++ TvRecyclerView/src/com/zzl/recyclerview/TvRecyclerView.java
@@ -0,0 +1,17 @@
+package com.zzl.recyclerview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+public class TvRecyclerView extends RecyclerView  {
+
+	public TvRecyclerView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
+
