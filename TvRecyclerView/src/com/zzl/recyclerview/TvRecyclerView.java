6b7e5af4f46ab620588e0957915aba4ca517b341
zzlnewair <zzl2007315@163.com>
2017-07-16 16:24:30 +0800

    TvRecyclerView init

--- TvRecyclerView/src/com/zzl/recyclerview/TvRecyclerView.java
+++ TvRecyclerView/src/com/zzl/recyclerview/TvRecyclerView.java
@@ -1,8 +1,10 @@
 package com.zzl.recyclerview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.view.View;
 
 public class TvRecyclerView extends RecyclerView  {
 
@@ -11,7 +13,35 @@ public class TvRecyclerView extends RecyclerView  {
 		// TODO Auto-generated constructor stub
 	}
 	
+	  public TvRecyclerView(Context context, AttributeSet attrs) {
+	        this(context, attrs, 0);
+	    }
 	
-	
+	  
+	  public TvRecyclerView(Context context, AttributeSet attrs, int defStyle) {
+	        super(context, attrs, defStyle);
+	        
+	        init(context);
+	        
+	     
+	    }
+	  
+	  
+	  private void init(Context context){
+	        setDescendantFocusability(FOCUS_BLOCK_DESCENDANTS);
+	        setChildrenDrawingOrderEnabled(true);
+	        setWillNotDraw(true); // 自身不作onDraw处理
+	        setHasFixedSize(true);
+	        setOverScrollMode(View.OVER_SCROLL_NEVER);
+
+	        setClipChildren(false);
+	        setClipToPadding(false);
+
+	        setClickable(false);
+	        setFocusable(true);
+	        setFocusableInTouchMode(true);
+	        
+	       
+	    }
 }
 
